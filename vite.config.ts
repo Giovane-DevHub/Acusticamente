4a827af710bb410ebd1230458e91b9d25a7f4ae2
Giovane-DevHub <giovane.wow3@gmail.com>
2026-09-15 14:00:06 -0300

    Add Arquivo do Vite.
    Adicionado o Vite e tsconfig.
    responsáveis por empacotar o código e conexão.
    atualizada as referencias.

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,  //Porta que vai rodar (http://localhost:3000)
+    open: false, //Não abre o navegador automaticamente
+    host: true   //Libera acesso a internet
+  }
+});
+
+//Vite.config.ts é responsável por:
+//Rodar o servidor de desenvolvimento com atualização em tempo real;
+//Compilar o código TypeScript, CSS e HTML.
